643af5e3e77d115a1c6e52f780aa27a310de4ed1
Li Xi <xl404@cam.ac.uk>
2017-08-10 21:58:09 +0100

    finished log in page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Nav, Platform } from 'ionic-angular';
 
-import { HomePage } from '../pages/home/home';
+import { DashboardPage } from '../pages/dashboard/dashboard';
 import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { SignupPage } from '../pages/signup/signup';
 import { UserPage } from '../pages/user/user';
 
 @Component({
@@ -13,7 +15,7 @@ import { UserPage } from '../pages/user/user';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = DashboardPage;
 
   pages: Array<{title: string, component: any}>;
 
@@ -22,9 +24,11 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage },
-      { title: 'User', component: UserPage }
+      { title: 'Dashboard', component: DashboardPage },
+      { title: 'Login', component: LoginPage },
+      { title: 'User', component: UserPage },
+      { title: "Signup", component: SignupPage},
+      { title: "List", component: ListPage}
     ];
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
-import { UserPage } from '../pages/user/user';
-import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { DashboardPage } from '../pages/dashboard/dashboard';
 import { ListPage } from '../pages/list/list';
-
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { LoginPage } from '../pages/login/login';
+import { SignupPage } from '../pages/signup/signup';
+import { UserPage } from '../pages/user/user';
+import { UserProvider } from '../providers/user/user';
+import { MyApp } from './app.component';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
     ListPage,
-    UserPage
+    UserPage,
+    LoginPage,
+    SignupPage, 
+    DashboardPage
   ],
   imports: [
     BrowserModule,
@@ -24,14 +28,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
     ListPage,
-    UserPage    
+    UserPage,
+    LoginPage,
+    SignupPage, 
+    DashboardPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UserProvider
   ]
 })
 export class AppModule {}
